11fbe86b50501694807f1b1d92810869b4e41aac
Laura Degiorgi <lunaticyber@MacBook-air-di-Laura.local>
2023-10-08 15:07:32 +0200

    correzione

--- emissione/src/main/java/it/unisalento/pas/emissione/repository/CheckingRepository.java
+++ emissione/src/main/java/it/unisalento/pas/emissione/repository/CheckingRepository.java
@@ -8,4 +8,5 @@ import org.springframework.stereotype.Repository;
 public interface CheckingRepository extends MongoRepository<Checking, String> {
 
     public Checking findByUsername(String username);
+    int deleteByUsername(String username);
 }
